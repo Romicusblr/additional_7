5d2feca91d4e21f440c211c4816e5c5c985db385
Romicusblr <mastroma@mail.ru>
2018-02-27 00:26:20 +0300

    add functions for bitmasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+    let maskForCol = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let maskForRow = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let maskForBox = [
+        [0, 0, 0, ],
+        [0, 0, 0, ],
+        [0, 0, 0, ]
+    ];
+
+    // create bitmasks
+
+    const bitpattern = (value) => {
+        return 1 << (value - 1)
+    };
+
+    const IsMoveLegal = (row, col, value) => {
+        let combinedMask = maskForNumbersSetInRow[row] | maskForNumbersSetInCol[col] | maskForNumbersSetInBox[boxRowNumber, boxColNumber];
+
+        return (matrix[row, col] == 0) && ((combinedMask & bitpattern(value)) == 0);
+
+    }
+
+    const insertValue = () => {
+        if (IsMoveLegal(row, col, value)) {
+            matrix[row, col] = value;
+            updateMask(row, col, value)
+        }
+    };
+
+    const updateMask = (row, col, value) => {
+        let bitpatternValue = bitpattern(value);
+
+        maskForRow[row] |= bitpatternValue;
+
+        maskForCol[col] |= bitpatternValue;
+
+        let boxRowNumber = row / 3 ^ 0;
+        let boxColNumber = col / 3 ^ 0;
+
+        maskForBox[boxRowNumber][boxColNumber] |= bitpatternValue;
+    }
+
+    matrix.forEach((elem, row) => {
+        return elem.forEach((elem, col) => {
+            if (elem) {
+                updateMask(row, col, elem);
+            }
+        })
+
+    });
+    debugger;
 }
